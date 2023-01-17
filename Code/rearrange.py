9f4b78deace09c4a7e62c4795fd2e3a99ac36e64
taymitsu <tmitsumori96@gmail.com>
2023-01-17 13:41:04 -0800

    first try rearrange commented out

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,9 +1,19 @@
-import random 
+#import random
+#cowlist = ["how", "now", "brown", "cow"]
+#random.shuffle(cowlist)
+
+#print(cowlist)
+
+#############################################
 
 #user input string of words 
 #join array of words, //.join(words)
 #rearrange with random.shuffle
 #print
+
+#############################################
+
+import random 
 
 def rearrange_words(list_of_words):
   random.shuffle(list_of_words)
@@ -11,6 +21,6 @@ def rearrange_words(list_of_words):
   print(result)
 
 if __name__ == '__main__':
-  words = input(str("Enter 4 words seperated by space: ")).split()
+  words = input(str("Enter 4 words: ")).split()
   rearrange_words(words)
 
